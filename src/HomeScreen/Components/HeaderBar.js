17549b825585f6633df754c765092f4521218e73
Zac Lew <zaclew922@gmail.com>
2021-10-22 16:29:42 +1100

    Changed to reactstrap header

--- src/HomeScreen/Components/HeaderBar.js
+++ src/HomeScreen/Components/HeaderBar.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "./HeaderBar.scss";
 
 const HeaderBar = (props) => {
   return (
-    <div className="header-bar">
-      <div className="menu">Menu</div>
-      <div className="welcome">Welcome Zac!</div>
-      <div className="view">Current View: Weekly</div>
-    </div>
+    <Navbar bg="light" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">Training Plan Viewer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Menu" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Edit Plan</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Export Plan
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav className="justify-content-end">
+            <NavDropdown
+              title={"Current View: " + "Weekly"}
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Daily</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Weekly</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Monthly</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
