087b464978824382104dafb65b5ea48c58dfc10e
Zac Lew <zaclew922@gmail.com>
2021-10-22 22:12:46 +1100

    Added swimming and strength training

--- src/HomeScreen/Components/DayCard.js
+++ src/HomeScreen/Components/DayCard.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./DayCard.scss";
 import { ReactComponent as Running } from "../../Assets/Icons/running.svg";
 import { ReactComponent as Cycling } from "../../Assets/Icons/cycling.svg";
+import { ReactComponent as Swimming } from "../../Assets/Icons/swimming.svg";
+import { ReactComponent as Strength } from "../../Assets/Icons/weightlifting.svg";
 import { capitalizeFirstLetter } from "../../Assets/Functions/utils";
 
 const DayCard = ({ dayOfWeek, data }) => {
@@ -24,7 +26,7 @@ const renderActivities = (data) => {
               key={activity.date + activity.daily_session}
             >
               <div>Session {activity.daily_session}:</div>
-              <div>{activity.distance + activity.distance_unit + " Run"}</div>
+              <div>{showDistanceOrTime(activity) + " Run"}</div>
               <Running />
               <div>Intensity: {capitalizeFirstLetter(activity.intensity)}</div>
             </div>
@@ -36,13 +38,43 @@ const renderActivities = (data) => {
               key={activity.date + activity.daily_session}
             >
               <div>Session {activity.daily_session}:</div>
-              <div>{activity.distance + activity.distance_unit + " Ride"}</div>
+              <div>{showDistanceOrTime(activity) + " Ride"}</div>
               <Cycling />
+              <div>Intensity: {capitalizeFirstLetter(activity.intensity)}</div>
+            </div>
+          );
+        case "swimming":
+          return (
+            <div
+              className="session"
+              key={activity.date + activity.daily_session}
+            >
+              <div>Session {activity.daily_session}:</div>
+              <div>{showDistanceOrTime(activity) + " Swim"}</div>
+              <Swimming />
+              <div>Intensity: {capitalizeFirstLetter(activity.intensity)}</div>
+            </div>
+          );
+        case "strength":
+          return (
+            <div
+              className="session"
+              key={activity.date + activity.daily_session}
+            >
+              <div>Session {activity.daily_session}:</div>
+              <div>{showDistanceOrTime(activity) + " Gym Session"}</div>
+              <Strength />
               <div>Intensity: {capitalizeFirstLetter(activity.intensity)}</div>
             </div>
           );
       }
     });
+};
+
+const showDistanceOrTime = (activity) => {
+  if (activity.distance) return activity.distance + activity.distance_unit;
+  else if (activity.time) return activity.time + "min";
+  else return "";
 };
 
 export default DayCard;
